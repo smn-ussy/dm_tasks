eb50edfb53aff1c47d4234cae9557fb755ad6bf9
smn-ussy <ryosuke_ushijima@so-netmedia.jp>
2025-04-17 10:37:35 +0900

    Update: 集計結果が元のSQLと一致するように修正

--- regular_tasks/monthly_tasks/MR/no3/sql/0-1.sql
+++ regular_tasks/monthly_tasks/MR/no3/sql/0-1.sql
@@ -6,19 +6,19 @@ WITH
       s.name ssp,
       ads.advertiser_id,
       advertiser_name,
-      case when user_agent_os_family in('iOS','Android') then 'SP' else 'PC' end os,
-      case when is_app is null then 0 else is_app end is_app,
-      case when creative_type in(5,6) then 'native' when creative_type in(10) then 'video' else 'display' end creative,
+      CASE WHEN user_agent_os_family IN ('iOS','Android') THEN 'SP' ELSE 'PC' END os,
+      CASE WHEN is_app IS NULL THEN 0 ELSE is_app END is_app,
+      CASE WHEN creative_type IN (5,6) THEN 'native' WHEN creative_type IN (10) THEN 'video' ELSE 'display' END creative,
       imp
-    from dm.domain_ads ads
-    inner join dm.hierarchies hie on ads.target_id = hie.target_id
-    inner join agency_console.campaign cp on ads.strategy_id = cp.real_strategy_id
-    inner join agency_console.campaign_agency_margin cam on cp.campaign_id = cam.campaign_id and concat_ws('-', year, month, day) between cam.start_date and cam.end_date
-    inner join agency_console.campaign_smn_margin csm on cp.campaign_id = csm.campaign_id and concat_ws('-', year, month, day) between csm.start_date and csm.end_date
-    left join console.ssp s on ads.child_ssp_id=s.id
-    where concat_ws('-', year, month, day) between '{start_at}' and '{end_at}'
-    and ads.advertiser_id not in({exclude_ads_id})
-    and child_ssp_id>0
+    FROM dm.domain_ads ads
+    INNER JOIN dm.hierarchies hie ON ads.target_id = hie.target_id
+    INNER JOIN agency_console.campaign cp ON ads.strategy_id = cp.real_strategy_id
+    INNER JOIN agency_console.campaign_agency_margin cam ON cp.campaign_id = cam.campaign_id AND concat_ws('-', year, month, day) between cam.start_date AND cam.end_date
+    INNER JOIN agency_console.campaign_smn_margin csm ON cp.campaign_id = csm.campaign_id AND concat_ws('-', year, month, day) between csm.start_date AND csm.end_date
+    LEFT JOIN console.ssp s ON ads.child_ssp_id=s.id
+    where concat_ws('-', year, month, day) between '{start_at}' AND '{end_at}'
+    AND ads.advertiser_id not IN ({exclude_ads_id})
+    AND child_ssp_id>0
   ),
   SummedImp AS (
     SELECT
@@ -27,9 +27,11 @@ WITH
       ssp,
       advertiser_id,
       advertiser_name,
-      SUM(CASE WHEN os = 'PC' AND is_app = 0 AND creative = 'display' THEN imp ELSE 0 END) AS sum_imp
+      SUM(imp) AS sum_imp
     FROM
       BaseData
+    WHERE
+      os = 'PC' AND is_app = 0 AND creative = 'display'
     GROUP BY
       ym,
       child_ssp_id,
